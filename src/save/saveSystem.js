c8ea88a5637b42e670d85dfd28e1fcc1c82b2b5f
LinjunJiang23 <mayljjiang@gmail.com>
2025-01-25 23:19:51 -0500

    ?

---
+++ src/save/saveSystem.js
@@ -0,0 +1,37 @@
+// src/save/saveSystem.js
+
+class SaveSystem {
+  constructor(eventManager) {
+	this.eventManager = eventManager;
+	this.autoSaveOn;
+	
+  }
+  
+  save_game_state() {
+  }
+  
+  load_game_state() {
+  }
+  
+  create_save_slot() {
+  }
+  
+  overwrite_save_slot() {
+  }
+  
+  delete_save_slot() {
+  }
+  
+  auto_save() {
+  }
+  
+  encrypt_save() {
+  }
+  
+  quick_save() {
+  }
+  
+  quick_load() {
+  }
+  
+};
